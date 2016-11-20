e3eee37c5291fdfa898f2e48dbab9dee2087bb47
CaiHuan <cai.huan25@gmail.com>
2016-11-21 00:52:06 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import {
+  AppRegistry
+} from 'react-native';
+import ReactNativeTypeScript from './build';
+
+AppRegistry.registerComponent('react_native_typescript', () => ReactNativeTypeScript);
